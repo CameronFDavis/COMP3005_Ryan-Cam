e67ad737fa624e83b5f3c764c3f9ce4693f213d5
Ryan Campbell <ryanjcampbell248@gmail.com>
2021-12-13 16:25:17 -0500

    Initial Commit v2

---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

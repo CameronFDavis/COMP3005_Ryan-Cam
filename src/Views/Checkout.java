09d9ce34cc71dfa3090d32ec6b7f265ebc9d7a47
Cameron Floyd Davis <50180480+CameronFDavis@users.noreply.github.com>
2021-12-19 15:18:41 -0500

    CheckoutSwing

--- src/Views/Checkout.java
+++ src/Views/Checkout.java
@@ -1,14 +1,13 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Tracking extends JDialog {
+public class Checkout extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
-    private JTextField txtOrderId;
-    private JList trackingList;
+    private JList checkoutList;
 
-    public Tracking() {
+    public Checkout() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
@@ -19,18 +18,24 @@ public class Tracking extends JDialog {
             }
         });
 
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                onOK();
+                onCancel();
             }
         });
 
         // call onCancel() on ESCAPE
         contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
@@ -40,8 +45,13 @@ public class Tracking extends JDialog {
         dispose();
     }
 
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
     public static void main(String[] args) {
-        Tracking dialog = new Tracking();
+        Checkout dialog = new Checkout();
         dialog.pack();
         dialog.setVisible(true);
         System.exit(0);

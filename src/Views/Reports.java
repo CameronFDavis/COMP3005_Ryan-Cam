bafa86201015f061fdd21e1414ff10527e108174
Cameron Floyd Davis <50180480+CameronFDavis@users.noreply.github.com>
2021-12-19 17:43:55 -0500

    ReportSwing

---
+++ src/Views/Reports.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Reports extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JPanel reportSpecsPanel;
+    private JPanel reportOutput;
+    private JButton createReportButton;
+    private JComboBox reportTypeBox;
+    private JComboBox sortByBox;
+    private JTable reportTable;
+
+    public Reports() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onOK();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+    
+    public static void main(String[] args) {
+        Reports dialog = new Reports();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
